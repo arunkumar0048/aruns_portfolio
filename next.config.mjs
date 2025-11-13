9da356b7a5c8aa7813096460c89571332f41d673
arun <arunkumarp0048@gmail.com>
2025-11-13 14:08:15 +0800

    Next.js static export + deploy setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // This ensures Next.js generates static HTML
+  basePath: '/aruns-portfolio', // use your repo name here
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
